70922cf02694d50eaba2278431e9ce9c00e3b40a
Mrinal Agrawal <agrawalmrinal21@gmail.com>
2026-03-10 16:32:28 +0530

    changed from local mongodb to mongodb atlas

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/sheryians_task';
+        const uri = process.env.MONGO_URI || 'mongodb+srv://agrawalmrinal21_db_user:pQBbcFde7TTsAUxL@shery.ou3v77p.mongodb.net/';
         await mongoose.connect(uri);
         console.log('MongoDB Connected');
     } catch (error) {
